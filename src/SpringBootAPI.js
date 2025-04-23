976bc3c59cc39753f0124f83b0a6e9b9fe34e1c2
Rashmi ranjan jena <ranjanjena8457@gmail.com>
2025-04-23 22:19:18 +0530

    Update base URL configuration to enable production API endpoint

--- src/SpringBootAPI.js
+++ src/SpringBootAPI.js
@@ -1,3 +1,3 @@
-// const base_url="https://sabita-kitchen-backend.onrender.com"; export default base_url;
+const base_url="https://sabita-kitchen-backend.onrender.com"; export default base_url;
 
-const base_url="http://localhost:8080"; export default base_url;
+// const base_url="http://localhost:8080"; export default base_url;
